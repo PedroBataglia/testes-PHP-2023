3b92ffa88e83da169e304013a6fd213aaef6455f
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-05-23 15:02:38 -0300

    não é necessário fazer usar essa função

--- DesafiosChatGPT/isPalindrome.php
+++ DesafiosChatGPT/isPalindrome.php
@@ -4,18 +4,12 @@ function isPalindrome($sentence)
 {
     $sentence = str_replace(" ", "", $sentence);
     $sentence = strtolower($sentence);
-    $palindrome = reverseString($sentence);
+    $palindrome = strrev($sentence);
     if ($palindrome == $sentence) {
         return TRUE;
     } else {
         return FALSE;
     }
-}
-
-function reverseString($sentence)
-{
-    $reverseSentence = strrev($sentence);
-    return $reverseSentence;
 }
 
 $palindrome = isPalindrome("subi no onibus");
