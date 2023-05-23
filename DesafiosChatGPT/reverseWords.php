5fbde4d2cc549fee211ea87f1b8e5ab6200a241b
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-05-23 09:25:52 -0300

    tornando o código mais bonito

--- DesafiosChatGPT/reverseWords.php
+++ DesafiosChatGPT/reverseWords.php
@@ -7,11 +7,12 @@ function reverseWord($palavra)
 
  function reverseWords($palavras)
  {
-    $palavraSeparadas = explode(" ", $palavras);
-    $palavraReversas = array_reverse($palavraSeparadas);
-    $palavraNovas = implode(" ", $palavraReversas);
-    return $palavraNovas;
-}
 
-//print_r(reverseWords("pizza Nova"));
+     $palavraSeparadas = preg_split('/\s+/', $palavras);
+     $palavraReversas = array_reverse($palavraSeparadas);
+     $palavraNovas = implode(" ", $palavraReversas);
+     return $palavraNovas;
+ }
+
+print_r(reverseWords("pizza Nova"));
 print_r(reverseWord("palavra"));
